ea3e848db69f6922f729cf8c42391c9cc899cece
avraj901 <avraj901@gmail.com>
2020-08-13 15:40:12 +0530

    Springapp3 using spring-app2 as a third party liberary

---
+++ spring-app3usingspringapp2jar-ThirdPartyjar/src/main/java/com/avanishit/spring_app3usingspringapp2jar_ThirdPartyjar/App.java
@@ -0,0 +1,13 @@
+package com.avanishit.spring_app3usingspringapp2jar_ThirdPartyjar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
